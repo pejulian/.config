88bc55c611acc2a86a68006a361c918207618607
pejulian <bleushade@gmail.com>
2024-05-10 11:57:23 +0800

    Update foldlevel

--- nvim/lua/pejulian/plugins/nvim-treesitter.lua
+++ nvim/lua/pejulian/plugins/nvim-treesitter.lua
@@ -11,7 +11,7 @@ return {
     vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
     vim.opt.foldtext = ""
     vim.opt.foldlevel = 99
-    vim.opt.foldlevelstart = 1
+    vim.opt.foldlevelstart = 20
     vim.opt.foldnestmax = 4
 
     -- zR - open all folds
