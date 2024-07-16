4ff28d1c672f027ef9fe107236e9bee0b262788f
Julian Pereira <bleushade@gmail.com>
2024-07-17 06:33:53 +0800

    Update which key mappings

--- nvim/lua/pejulian/plugins/which-key.lua
+++ nvim/lua/pejulian/plugins/which-key.lua
@@ -9,6 +9,13 @@ return {
     local wk = require("which-key")
 
     wk.add({
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
       { "<leader>g", group = "git" },
       { "<leader>gc", group = "copilot" },
       { "<leader>gi", group = "git" },
