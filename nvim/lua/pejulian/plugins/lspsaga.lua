8e2e5863e3760a5affdcec4341046d49116ba4cd
pejulian <bleushade@gmail.com>
2024-07-30 11:04:52 +0800

    Add lspsaga jump to next/prev diagnostics

--- nvim/lua/pejulian/plugins/lspsaga.lua
+++ nvim/lua/pejulian/plugins/lspsaga.lua
@@ -1,29 +1,31 @@
 return {
-  "nvimdev/lspsaga.nvim",
-  event = { "LspAttach" },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("lspsaga").setup({})
+    "nvimdev/lspsaga.nvim",
+    event = { "LspAttach" },
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("lspsaga").setup({})
 
-    local wk = require("which-key")
+        local wk = require("which-key")
 
-    wk.add({
-      { "<leader>l", group = "lsp" },
-      { "<leader>ls", group = "saga" },
-      { "<leader>lsD", "<cmd>Lspsaga goto_type_definition<cr>", desc = "Goto type definition" },
-      { "<leader>lsP", "<cmd>Lspsaga peek_type_definition<cr>", desc = "Peek type definition" },
-      { "<leader>lsc", "<cmd>Lspsaga code_action<cr>", desc = "Code action" },
-      { "<leader>lsd", "<cmd>Lspsaga goto_definition<cr>", desc = "Goto definition" },
-      { "<leader>lsf", "<cmd>Lspsaga finder<cr>", desc = "LSP Finder" },
-      { "<leader>lsh", "<cmd>Lspsaga hover_doc<cr>", desc = "Hover doc" },
-      { "<leader>lso", "<cmd>Lspsaga outline<cr>", desc = "Outline" },
-      { "<leader>lsp", "<cmd>Lspsaga peek_definition<cr>", desc = "Peek definition" },
-      { "<leader>lsr", "<cmd>Lspsaga rename<cr>", desc = "Rename" },
-      { "<leader>lst", "<cmd>Lspsaga term_toggle<cr>", desc = "Terminal toggle" },
-      { "<leader>lsw", "<cmd>Lspsaga show_workspace_diagnostics<cr>", desc = "Show Workspace Diagnostics" },
-    })
-  end,
+        wk.add({
+            { "<leader>l", group = "lsp" },
+            { "<leader>ls", group = "saga" },
+            { "<leader>lsD", "<cmd>Lspsaga goto_type_definition<cr>", desc = "Goto type definition" },
+            { "<leader>lsP", "<cmd>Lspsaga peek_type_definition<cr>", desc = "Peek type definition" },
+            { "<leader>lsc", "<cmd>Lspsaga code_action<cr>", desc = "Code action" },
+            { "<leader>lsd", "<cmd>Lspsaga goto_definition<cr>", desc = "Goto definition" },
+            { "<leader>lsf", "<cmd>Lspsaga finder<cr>", desc = "LSP Finder" },
+            { "<leader>lsh", "<cmd>Lspsaga hover_doc<cr>", desc = "Hover doc" },
+            { "<leader>lso", "<cmd>Lspsaga outline<cr>", desc = "Outline" },
+            { "<leader>lsp", "<cmd>Lspsaga peek_definition<cr>", desc = "Peek definition" },
+            { "<leader>lsr", "<cmd>Lspsaga rename<cr>", desc = "Rename" },
+            { "<leader>lst", "<cmd>Lspsaga term_toggle<cr>", desc = "Terminal toggle" },
+            { "<leader>lsw", "<cmd>Lspsaga show_workspace_diagnostics<cr>", desc = "Show Workspace Diagnostics" },
+            { "<leader>lsj", "<cmd>Lspsaga diagnostic_jump_next<cr>", desc = "Jump to next diagnostic" },
+            { "<leader>lsk", "<cmd>Lspsaga diagnostic_jump_prev<cr>", desc = "Jump to previous diagnostic" },
+        })
+    end,
 }
