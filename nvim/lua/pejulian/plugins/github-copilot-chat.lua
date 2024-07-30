8e73e45705eaf69e7b80e99c1bcf942ab2b1761f
pejulian <bleushade@gmail.com>
2024-07-30 10:18:18 +0800

    Update copilot chat integrations

--- nvim/lua/pejulian/plugins/github-copilot-chat.lua
+++ nvim/lua/pejulian/plugins/github-copilot-chat.lua
@@ -1,43 +1,58 @@
 return {
-  "CopilotC-Nvim/CopilotChat.nvim",
-  branch = "canary",
-  dependencies = {
-    { "github/copilot.vim" },
-    { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-  },
-  opts = {
-    debug = true, -- Enable debugging
-  },
-  keys = {
-    {
-      "<leader>gcq",
-      function()
-        vim.ui.input({
-          prompt = "Quick chat: ",
-          completion = "file",
-        }, function(value, canceled)
-          if canceled then
-            return
-          else
-            require("CopilotChat").ask(value, { selection = require("CopilotChat.select").buffer })
-          end
-        end)
-      end,
-      desc = "GitHub Copilot Chat - Quick Chat",
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+        { "zbirenbaum/copilot.lua" },
+        { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
-    {
-      "<leader>gch",
-      function()
-        require("CopilotChat.integrations.telescope").pick(require("CopilotChat.actions").help_actions())
-      end,
-      desc = "GitHub Copilot Chat - Prompt actions",
+    opts = {
+        debug = true, -- Enable debugging
+        allow_insecure = true, -- Allow insecure connections
+        window = {
+            layout = "float",
+            relative = "cursor",
+            width = 1,
+            height = 0.4,
+            row = 1,
+        },
+        mappings = {
+            complete = {
+                insert = "",
+            },
+        },
     },
-    {
-      "<leader>gcp",
-      function()
-        require("CopilotChat.integrations.telescope").pick(require("CopilotChat.actions").prompt_actions())
-      end,
-      desc = "GitHub Copilot Chat - Prompt actions",
+    keys = {
+        {
+            "<leader>gcq",
+            function()
+                vim.ui.input({
+                    prompt = "Quick chat: ",
+                    completion = "file",
+                }, function(value, canceled)
+                    if canceled then
+                        return
+                    else
+                        require("CopilotChat").ask(value, { selection = require("CopilotChat.select").buffer })
+                    end
+                end)
+            end,
+            desc = "GitHub Copilot Chat - Quick Chat",
+        },
+        {
+            "<leader>gch",
+            function()
+                local telescope = require("CopilotChat.integrations.telescope")
+                local actions = require("CopilotChat.actions").help_actions()
+                telescope.pick(actions)
+            end,
+            desc = "GitHub Copilot Chat - Help actions",
+        },
+        {
+            "<leader>gcp",
+            function()
+                require("CopilotChat.integrations.telescope").pick(require("CopilotChat.actions").prompt_actions())
+            end,
+            desc = "GitHub Copilot Chat - Prompt actions",
+        },
     },
-  },
 }
