179a08027df5f79fa28e017a99d31064d18a26e7
pejulian <bleushade@gmail.com>
2024-08-07 15:31:46 +0800

    Add binding to open terminal

--- nvim/lua/pejulian/plugins/which-key.lua
+++ nvim/lua/pejulian/plugins/which-key.lua
@@ -1,37 +1,38 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
-  config = function()
-    local wk = require("which-key")
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 500
+    end,
+    config = function()
+        local wk = require("which-key")
 
-    wk.add({
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer Local Keymaps (which-key)",
-      },
-      { "<leader>g", group = "git" },
-      { "<leader>gc", group = "copilot" },
-      { "<leader>gi", group = "git" },
-      { "<leader>s", group = "splits" },
-      { "<leader>se", "<C-w>=", desc = "Make splits equal size" },
-      { "<leader>sh", "<C-w>s", desc = "Split window horizontally" },
-      { "<leader>sv", "<C-w>v", desc = "Split window vertically" },
-      { "<leader>sx", "<cmd>close<CR>", desc = "Close current split" },
-      { "<leader>t", group = "tabs" },
-      { "<leader>tf", "<cmd>tabnew %<CR>", desc = "Open current buffer in new tab" },
-      { "<leader>tn", "<cmd>tabn<CR>", desc = "Go to next tab" },
-      { "<leader>to", "<cmd>tabnew<CR>", desc = "Open new tab" },
-      { "<leader>tp", "<cmd>tabp<CR>", desc = "Go to previous tab" },
-      { "<leader>tx", "<cmd>tabclose<CR>", desc = "Close current tab" },
-      { "<leader>x", group = "trouble" },
-    })
-  end,
-  opts = {},
+        wk.add({
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+            { "<leader>g", group = "git" },
+            { "<leader>gc", group = "copilot" },
+            { "<leader>gi", group = "git" },
+            { "<leader>s", group = "splits" },
+            { "<leader>se", "<C-w>=", desc = "Make splits equal size" },
+            { "<leader>sh", "<C-w>s", desc = "Split window horizontally" },
+            { "<leader>sv", "<C-w>v", desc = "Split window vertically" },
+            { "<leader>sx", "<cmd>close<CR>", desc = "Close current split" },
+            { "<leader>t", group = "tabs" },
+            { "<leader>tf", "<cmd>tabnew %<CR>", desc = "Open current buffer in new tab" },
+            { "<leader>tn", "<cmd>tabn<CR>", desc = "Go to next tab" },
+            { "<leader>to", "<cmd>tabnew<CR>", desc = "Open new tab" },
+            { "<leader>tp", "<cmd>tabp<CR>", desc = "Go to previous tab" },
+            { "<leader>tx", "<cmd>tabclose<CR>", desc = "Close current tab" },
+            { "<leader>x", group = "trouble" },
+            { "<leader>T", "<cmd>:terminal<CR>", desc = "Open terminal as a new tab" },
+        })
+    end,
+    opts = {},
 }
